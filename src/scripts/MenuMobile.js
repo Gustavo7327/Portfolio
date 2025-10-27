b1d63292d4af1eb80a11e86127a73ad45d3a2184
Gustavo Chaves <gustavosilva712493@gmail.com>
2025-10-27 17:37:33 -0300

    feat: script for control the mobile menu

---
+++ src/scripts/MenuMobile.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btnMenu = document.getElementById("menu-mobile");
+    const menuMobile = document.getElementById("menu-mobile-links");
+    const linksMobile = document.querySelectorAll(".link");
+
+    linksMobile.forEach((link) => {
+        link.addEventListener("click", () => {
+        menuMobile.classList.remove("translate-y-0");
+        menuMobile.classList.add("translate-y-[200vh]");
+        });
+    });
+
+    btnMenu.addEventListener("click", () => {
+        const isOpen = menuMobile.classList.contains("translate-y-[200vh]");
+
+        if (isOpen) {
+        menuMobile.classList.remove("translate-y-[200vh]");
+        menuMobile.classList.add("translate-y-0");
+        } else {
+        menuMobile.classList.remove("translate-y-0");
+        menuMobile.classList.add("translate-y-[200vh]");
+        }
+    });
+
+    document.addEventListener("click", (e) => {
+        if (!btnMenu.contains(e.target) && !menuMobile.contains(e.target)) {
+        menuMobile.classList.remove("translate-y-0");
+        menuMobile.classList.add("translate-y-[200vh]");
+        }
+    });
+});
