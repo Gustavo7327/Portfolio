a85b65452598948c49e3d481e977d62851994442
Gustavo Chaves <gustavosilva712493@gmail.com>
2025-10-27 17:39:35 -0300

    feat: adding initTheme and poppins font

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "@fontsource/poppins";
 import './index.css'
 import App from './App.jsx'
+import { initTheme } from './scripts/Theme';
+
+initTheme();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
