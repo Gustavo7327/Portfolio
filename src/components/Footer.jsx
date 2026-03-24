226b448a30d0ccf2216f61827742c2d68633911b
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-03-23 21:43:14 -0300

    feat: useLanguage implementation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
+import { useLanguage } from "../i18n/LanguageProvider";
+
 export default function Footer() {
+  const { t } = useLanguage();
+
   return (
     <footer className="w-full px-3 py-4 mt-auto">
       <p className="text-black dark:text-white text-sm text-center">
-        &copy; {new Date().getFullYear()} Gustavo Chaves. Todos os direitos
-        reservados.
+        &copy; {new Date().getFullYear()} Gustavo Chaves. {t("footer.copyright")}
       </p>
     </footer>
   );
