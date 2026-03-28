b842f1d36e7fd7e308e4bb5023a2eb55d63ca640
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-03-28 00:38:54 -0300

    feat: add border and bg color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   const { t } = useLanguage();
 
   return (
-    <footer className="w-full px-3 py-4 mt-auto">
+    <footer className="w-full px-3 py-4 mt-auto bg-gray-50 dark:bg-gray-800 border-t-3 border-gray-200 dark:border-gray-900">
       <p className="text-black dark:text-white text-sm text-center">
         &copy; {new Date().getFullYear()} Gustavo Chaves. {t("footer.copyright")}
       </p>
