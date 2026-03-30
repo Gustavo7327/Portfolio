1710a006e1821b4860089269b4a56c5c7ea8bb08
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-03-30 01:04:16 -0300

    feat: update translations file

--- src/i18n/translations.js
+++ src/i18n/translations.js
@@ -23,6 +23,26 @@ export const translations = {
       modal: {
         technologies: "Tecnologias Utilizadas",
         github_link: "Ver no GitHub",
+      },
+      items: {
+        1: {
+          description: "O Doeit é um sistema de doações web que conecta doadores e donatários de forma ágil e segura. O sistema permite a validação em tempo real de cada doação por meio de QR Codes e utiliza tecnologias modernas para garantir uma experiência fluida e intuitiva."
+        },
+        2: {
+          description: "O ByteTech é uma plataforma simples e intuitiva que permite ser realizado a leitura e escrita de publicações, artigos ou tutoriais sobre tecnologia. Integrado com um editor de texto rico e com uma interface construída em TailwindCSS e Blade, é possível navegar e realizar ações com facilidade."
+        },
+        3: {
+          description: "API para exibição de produtos de pequenos negócios locais feita com Spring Boot."
+        },
+        4: {
+          description: "O projeto é um piano com o objetivo de ensinar a tocar músicas caso não tenha nenhuma experiência com notas musicais. Com luzes de LED guiando o usuário, o piano permite que você toque nota por nota da música embarcada."
+        },
+        5: {
+          description: "Sistema de administração e catálogo de vacinas disponíveis para aplicação em postos de saúde da cidade."
+        },
+        6: {
+          description: "O projeto tem como objetivo bloquear sites manipulando o arquivo hosts do linux, redirecionando domínios indesejados para a própria máquina, resultando em um erro na conexão entre o site. O projeto foi desenvolvido utilizando o Qt Framework em C++."
+        }
       }
     },
     certificates: {
@@ -31,6 +51,86 @@ export const translations = {
       view_details: "Ver detalhes",
       modal: {
         concepts: "Conceitos Abordados",
+      },
+      items: {
+        1: {
+          issueDate: "Junho de 2025",
+          concepts: [
+            "Definição de análise de dados",
+            "Pensamento analítico",
+            "Tipos, estruturas e ciclo de vida dos dados",
+            "Ética na análise de dados",
+            "Definição do problema e identificação de stakeholders, transformando em perguntas específicas e mensuráveis",
+            "Como usar os dados para tomar decisões mais assertivas",
+            "Ferramentas e funções essenciais para organizar, limpar e analisar dados em planilhas",
+            "Fórmulas e criação de gráficos para visualizar dados",
+            "Comunicação de forma eficaz",
+            "Tipos e estruturas de dados",
+            "Responabilidade dos dados",
+            "Fundamentos de banco de dados",
+            "Organizar e proteger dados",
+            "Importância da integridade",
+            "Limpeza de dados para insights mais precisos",
+            "Limpeza de dados com SQL",
+            "Verificação e relato dos resultados da limpeza",
+            "Organização dos dados com classificações e filtros",
+            "Formatação e conversão de dados",
+            "Funções de planilha para realizar cálculos e manipulações de dados",
+            "Agregação de dados com SQL e planilhas",
+            "Tabelas dinâmicas",
+            "Visualizações de dados: tipos, princípios, elementos",
+            "Tableau",
+            "Apresentação de dados com história convincente e impactante",
+            "Melhores práticas para criação de visualizações",
+            "Linguagem R e suas vantagens para análise de dados",
+            "Como usar o R Studio",
+            "Sintaxe básica da linguagem R e conceitos de programação",
+            "O que são Dataframes e como utilizá-los",
+            "Análise de dados com R",
+            "Visualização de dados com R",
+            "Como usar o R Markdown"
+          ]
+        },
+        2: {
+          issueDate: "Março de 2025",
+          concepts: [
+            "Visão geral dos conceitos em nuvem",
+            "Economia e faturamento da nuvem",
+            "Visão geral da infraestrutura global da AWS e dos seus serviços",
+            "Segurança na nuvem AWS",
+            "Redes e entrega de conteúdo",
+            "Serviços de computação da AWS",
+            "Serviços de armazenamento da AWS",
+            "Serviços de banco de dados da AWS",
+            "Arquitetura de nuvem",
+            "Auto scaling e monitoramento"
+          ]
+        },
+        3: {
+          issueDate: "Setembro de 2024",
+          concepts: [
+            "Versionamento de código com Git e GitHub",
+            "Fundamentos e pilares da Programação Orientada a Objetos",
+            "Java Collections",
+            "Java Stream API",
+            "Modelagem UML",
+            "Bancos de dados relacionais (SQL)",
+            "Banco de dados NoSQL",
+            "Gerenciamento de dependências com Maven",
+            "Testes unitários com JUnit",
+            "Spring Framework com Spring Boot",
+            "Programação assistida por IA com Amazon CodeWhisperer",
+            "Criação de testes unitários com IA generativa",
+            "API REST com Spring Web e Swagger",
+            "Segurança de API REST com Spring Security",
+            "Introdução a computação em nuvem com AWS",
+            "Introdução à Amazon Web Services (AWS)",
+            "Armazenamento e Banco de Dados na AWS",
+            "Segurança, Monitoramento e Suporte na AWS",
+            "Fundamentos da Cloud Computing com AWS",
+            "Design Patterns"
+          ]
+        }
       }
     },
     switch: {
@@ -70,6 +170,26 @@ export const translations = {
       modal: {
         technologies: "Technologies Used",
         github_link: "View on GitHub",
+      },
+      items: {
+        1: {
+          description: "Doeit is a web donation system that connects donors and recipients quickly and securely. The system supports real-time validation of each donation through QR Codes and uses modern technologies to ensure a smooth and intuitive experience."
+        },
+        2: {
+          description: "ByteTech is a simple and intuitive platform that allows reading and writing posts, articles, or tutorials about technology. Integrated with a rich text editor and a UI built with TailwindCSS and Blade, it is easy to navigate and perform actions."
+        },
+        3: {
+          description: "API to display products from small local businesses built with Spring Boot."
+        },
+        4: {
+          description: "The project is a piano designed to teach playing songs even without any prior knowledge of musical notes. With LED lights guiding the user, the piano allows note-by-note playback of the embedded song."
+        },
+        5: {
+          description: "Vaccine Management and catalog system for available vaccines at city health posts."
+        },
+        6: {
+          description: "The project aims to block websites by manipulating the Linux hosts file, redirecting unwanted domains to the local machine, causing connection errors. The project was developed using Qt Framework in C++."
+        }
       }
     },
     certificates: {
@@ -78,6 +198,86 @@ export const translations = {
       view_details: "View Details",
       modal: {
         concepts: "Concepts Covered",
+      },
+      items: {
+        1: {
+          issueDate: "June 2025",
+          concepts: [
+            "Definition of data analysis",
+            "Analytical thinking",
+            "Types, structures and data lifecycle",
+            "Ethics in data analysis",
+            "Problem definition and stakeholder identification, turning it into specific and measurable questions",
+            "How to use data to make more assertive decisions",
+            "Essential tools and functions to organize, clean and analyze data in spreadsheets",
+            "Formulas and chart creation to visualize data",
+            "Effective communication",
+            "Types and structures of data",
+            "Data responsibility",
+            "Database fundamentals",
+            "Organizing and protecting data",
+            "Importance of integrity",
+            "Data cleaning for more accurate insights",
+            "Data cleaning with SQL",
+            "Verification and reporting of cleaning results",
+            "Data organization with sorting and filters",
+            "Formatting and conversion of data",
+            "Spreadsheet functions for calculations and transformations",
+            "Data aggregation with SQL and spreadsheets",
+            "Pivot tables",
+            "Data visualizations: types, principles, elements",
+            "Tableau",
+            "Presenting data with a compelling and impactful story",
+            "Best practices for creating visualizations",
+            "R language and its advantages for data analysis",
+            "How to use R Studio",
+            "Basic R syntax and programming concepts",
+            "What are Dataframes and how to use them",
+            "Data analysis with R",
+            "Data visualization with R",
+            "How to use R Markdown"
+          ]
+        },
+        2: {
+          issueDate: "March 2025",
+          concepts: [
+            "Cloud concepts overview",
+            "Cloud economics and billing",
+            "Global AWS infrastructure and services overview",
+            "AWS cloud security",
+            "Networking and content delivery",
+            "AWS compute services",
+            "AWS storage services",
+            "AWS database services",
+            "Cloud architecture",
+            "Auto scaling and monitoring"
+          ]
+        },
+        3: {
+          issueDate: "September 2024",
+          concepts: [
+            "Code versioning with Git and GitHub",
+            "Object-Oriented Programming fundamentals and pillars",
+            "Java Collections",
+            "Java Stream API",
+            "UML modeling",
+            "Relational databases (SQL)",
+            "NoSQL databases",
+            "Dependency management with Maven",
+            "Unit tests with JUnit",
+            "Spring Framework with Spring Boot",
+            "AI-assisted coding with Amazon CodeWhisperer",
+            "Creating unit tests with generative AI",
+            "REST API with Spring Web and Swagger",
+            "REST API security with Spring Security",
+            "Introduction to cloud computing with AWS",
+            "Introduction to Amazon Web Services (AWS)",
+            "AWS storage and database",
+            "AWS security, monitoring and support",
+            "AWS cloud computing fundamentals",
+            "Design Patterns"
+          ]
+        }
       }
     },
     switch: {
